7052089d5a0a9eda0d03e9bb5d6786bee2df5eca
WisdomB1 <162624280+WisdomB1@users.noreply.github.com>
2025-02-06 10:22:41 -0500

    eportfolio update

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// Open menu function
 function openMenu() {
     document.querySelector('body').classList.add('menu--open');
 }
 
+// Close menu function
 function closeMenu() {
     document.querySelector('body').classList.remove('menu--open');
 }
